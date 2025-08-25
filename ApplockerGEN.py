52433719925570eab9b58f3ddae57144b5112a8a
Michael Haag <5632822+MHaggis@users.noreply.github.com>
2025-08-25 06:35:57 -0600

    link fixes

--- ApplockerGEN.py
+++ ApplockerGEN.py
@@ -14,20 +14,18 @@ with col1:
 
     Key features of AppLockerGen include:
 
-    1️⃣ <a href="AppLocker_Scripts_and_Paths_Policy" target="_self">**Policy Creation**</a>: Easily define rules for your applications, scripts, and installers to enhance security. This page allows you to specify rules for paths and scripts, and choose whether to set them to 'Audit' or 'Block' mode. 📝
+    1️⃣ <a href="Applocker_Scripts_and_Paths_Policy" target="_self">**Policy Creation**</a>: Easily define rules for your applications, scripts, and installers to enhance security. This page allows you to specify rules for paths and scripts, and choose whether to set them to 'Audit' or 'Block' mode. 📝
 
-    2️⃣ <a href="App:ocker_Combiner" target="_self">**Policy Merging**</a>: Combine multiple AppLocker policies into one comprehensive set of rules. This tool helps you combine multiple AppLocker policies into a single policy file. 🧩
+    2️⃣ <a href="Applocker_Combiner" target="_self">**Policy Merging**</a>: Combine multiple AppLocker policies into one comprehensive set of rules. This tool helps you combine multiple AppLocker policies into a single policy file. 🧩
 
     3️⃣ <a href="AppLocker_Pre-Built_Policies" target="_self">**Pre-Built Policies**</a>: Ensure that your AppLocker policies are correctly formatted and free of errors before deployment. This application allows you to download pre-created policies to block common applications. You can also modify the policies to suit your needs. ✅
 
-    4️⃣ <a href="Modify_AppLocker_Policy" target="_self">**Export and Import**</a>: Import existing policies for editing, and export your policies for deployment across your organization. This page allows you to upload an AppLocker Policy XML file or paste your AppLocker Policy XML for modification. 🔄
+    4️⃣ <a href="Modify_Applocker_Policy" target="_self">**Export and Import**</a>: Import existing policies for editing, and export your policies for deployment across your organization. This page allows you to upload an AppLocker Policy XML file or paste your AppLocker Policy XML for modification. 🔄
     
-    5️⃣ <a href="AppLocker_Essentials" target="_self">**Learn More About AppLocker**</a>: This page provides a brief overview of AppLocker, including the different types of rules, and how to configure AppLocker. 📊
+    5️⃣ <a href="Applocker_Essentials" target="_self">**Learn More About AppLocker**</a>: This page provides a brief overview of AppLocker, including the different types of rules, and how to configure AppLocker. 📊
     
     6️⃣ <a href="AppLocker_Inspector" target="_self">**AppLocker Inspector**</a>: Audit your AppLocker policies for security misconfigurations and get actionable recommendations. Collaboration with Spencer Alessi (@techspence). 🔍
     
-    7️⃣ <a href="AppLocker_Atomic_Testing" target="_self">**AppLocker Atomic Testing**</a>: This page provides a list of common AppLocker bypasses and how to test them. 📄
-
     AppLockerGen is an open-source project, and we encourage contributions and feedback from the community to continue improving the tool. 🌐
 
     Get started by uploading your existing AppLocker XML files or create new policies using the interface provided. 📤📥
